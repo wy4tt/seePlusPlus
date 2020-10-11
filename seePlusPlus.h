814582b90572a0f8d7991c2e3899b5e7261f335d
Wyatt Houston <h.wy4tt@gmail.com>
2020-10-10 22:00:32 -0600

    Add files via upload

---
+++ seePlusPlus.h
@@ -0,0 +1,16 @@
+//
+//  abbreviations.h
+//  seePlusPluss
+//
+//  Created by Wyatt Houston on 10/10/20.
+//
+
+#ifndef abbreviations_h
+#define abbreviations_h
+
+#define 👐 {
+#define 🙏 }
+#define 🤮 return
+
+
+#endif /* abbreviations_h */
